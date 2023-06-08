7c83acd1eb6d19d395f040eedf02a730d5a61316
Samira Boudjemai <sboudjemai@xpanxion.com>
2023-06-08 14:22:00 -0400

    update ex7

--- server/index.js
+++ server/index.js
@@ -22,11 +22,11 @@ app.get('/person/:id', cors(corsOptions), async (req, res) => {
     // const personType = req.query['personType']   // Read query parameters from URL.
     // const body = req.body                        // Read request body.
     // res.send(<YOUR OBJECT HERE>)
-    res.send({message: 'Hello World!'})
+    res.send({message: 'Hey World'})
 })
 
 app.get('/message', cors(corsOptions), async(req, res) => {
-    res.send({message: 'Hi my name is Samira!'})
+    res.send({message: 'Hi my name is Nico Portalatin!!!'})
 })
 
 app.get('/persons/', cors(corsOptions), async (req,res) => {
@@ -42,40 +42,75 @@ app.get('/persons/', cors(corsOptions), async (req,res) => {
 
 
 // Car ID
-app.get('/car/:id', cors(corsOptions), async (req, res) => { 
+app.get('/car/:id', cors(corsOptions), param('id').isNumeric(), async (req, res) => { 
+
+  const errors = validationResult(req)
+
+        if (!errors.isEmpty()) {
+           return res.status(400).json({ errors: errors.array() })  
+        }
   const carId = req.params['id']
   const car = await mySqlProxy.selectCarById(carId)
+  car ? res.send(car) : res.status(404).send({message: 'Not found.'})
+  
   res.send(car)
 })
 
 //Car Make 
 app.get('/cars', cors(corsOptions), async (req,res) => {
+
+  if (!errors.isEmpty()) {
+    return res.status(400).json({ errors: errors.array() })  
+ }
+  
   const make = req.query.make
   const cars = await mySqlProxy.selectCarByMake(make);
+  cars ? res.send(cars) : res.status(404).send({message: 'Not found.'})
   res.send(cars);
 
 })
 
 //Car post (adding)
 app.post('/cars/', cors(corsOptions), async (req, res) => { 
+
+  if (!errors.isEmpty()) {
+    return res.status(400).json({ errors: errors.array() })  
+ }
+
   const car = req.body 
   const newCar = await mySqlProxy.insertCar(car)
+  newCar ? res.send(newCar) : res.status(404).send({message: 'Not found.'})
   res.send(newCar)
 })
 
 //Car Put
 
 app.put('/cars', cors(corsOptions), async (req, res) => { 
+
+  if (!errors.isEmpty()) {
+    return res.status(400).json({ errors: errors.array() })  
+ }
+
   const car = req.body 
   const newCar = await mySqlProxy.updateCar(car)
+  newCar ? res.send(newCar) : res.status(404).send({message: 'Not found.'})
   res.send(newCar);
 })
 
-app.delete('/cars/:id', cors(corsOptions), async (req, res) => {
+app.delete('/cars/:id', cors(corsOptions), param('id').isNumeric(), async (req, res) => {
+
+          // Validate...
+        const errors = validationResult(req)
+        if (!errors.isEmpty()) {
+            return res.status(400).json({ errors: errors.array() })
+        }
+
     const carId = req.params['id']
     const results = await mySqlProxy.deleteCar(carId)
+    results ? res.send(results) : res.status(404).send({message: 'Not found.'})
     res.send(results)
 })
+
 
 
 app.listen(PORT, () => {
